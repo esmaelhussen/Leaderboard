17475dd13d1d958d6ccd3540fc54b98cb539f489
esmael hussen abdu <esmaelhussenabdu@gmail.com>
2025-01-07 21:13:12 +0300

    adjust the js files structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import _ from 'lodash';
 import './style.css';
-import { renderScores, addScore } from './function.js';
 
 const scores = [];
+
+const addScore = (name, score, scores) => {
+  if (name && score) {
+    scores.push({ name, score });
+  }
+};
+
+const renderScores = (scores) => {
+  const scoresList = document.getElementById('scores-list');
+  scoresList.innerHTML = '';
+  scores.forEach((score) => {
+    const li = document.createElement('li');
+    li.textContent = `${score.name}: ${score.score}`;
+    li.classList.add('score-item');
+    scoresList.appendChild(li);
+  });
+};
 
 document.getElementById('refresh').addEventListener('click', () => {
   renderScores(scores);
