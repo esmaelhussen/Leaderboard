5cef68831d907a6ccd9cd45854b2ac5666a67bcd
esmael hussen abdu <esmaelhussenabdu@gmail.com>
2025-02-04 20:51:52 +0300

    create new game to remove the past records

--- src/function.js
+++ src/function.js
@@ -1,4 +1,5 @@
-const API_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ze1TjZFGUQAXVEhzCJJJ/scores/';
+const API_URL =
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ KNupmnCZz49JFSVJgwYr/scores/";
 
 export const getGame = async () => {
   const fetchGame = await fetch(API_URL);
@@ -8,10 +9,36 @@ export const getGame = async () => {
 
 export const postGame = async (score) => {
   await fetch(API_URL, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(score),
   });
 };
+
+/*
+//post method for creating a game
+
+const creategame = async () => {
+  const url =
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/";
+  const data = { name: "esmael game" };
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error("Error creating game:", error);
+  }
+};
+creategame();
+*/
