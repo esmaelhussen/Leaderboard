6a6a3a9da093445992458c92e256310674b6e51b
esmael hussen abdu <esmaelhussenabdu@gmail.com>
2025-02-04 21:06:28 +0300

    make the name meaningful

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
-import { postGame, getGame } from './function.js';
-import './style.css';
+import { postGame, getGame } from "./function.js";
+import "./style.css";
 
-const allScore = document.querySelector('.scores');
-const nameone = document.querySelector('.name');
-const scoreone = document.querySelector('.score');
-const submitButon = document.querySelector('.btn-submit');
-const refreshButon = document.querySelector('.btn-refresh');
+const allScore = document.querySelector(".scores");
+const name = document.querySelector(".name");
+const score = document.querySelector(".score");
+const submitButon = document.querySelector(".btn-submit");
+const refreshButon = document.querySelector(".btn-refresh");
 
-const addScore = async (event) => {
-  event.preventDefault();
-  await postGame({ user: nameone.value, score: +scoreone.value });
-  nameone.value = '';
-  scoreone.value = '';
+const addScore = async () => {
+  await postGame({ user: name.value, score: score.value });
+  name.value = "";
+  score.value = "";
 };
 
 const renderScores = async () => {
   const scores = await getGame();
-  allScore.innerHTML = '';
+  allScore.innerHTML = "";
   scores.forEach(({ user, score }) => {
     allScore.innerHTML += `<li class="score__list">${user} : ${score}</li>`;
   });
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   renderScores();
 });
 
-submitButon.addEventListener('click', addScore);
-refreshButon.addEventListener('click', renderScores);
+submitButon.addEventListener("click", addScore);
+refreshButon.addEventListener("click", renderScores);
